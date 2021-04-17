3018fb799e6ac6ccd8e7d40bcf9e0c31170cb3fd
Fabian Frangella <fabianfrangella@gmail.com>
2021-04-17 14:28:42 -0300

    basic stats methods

--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -4,6 +4,13 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    public int health;
+    public int mana;
+    public int stamina;
+    public int maxHealth;
+    public int maxMana;
+    public int maxStamina;
+    public event System.Action OnPlayerDeath;
 
     // Start is called before the first frame update
     void Start()
@@ -12,13 +19,65 @@ public class PlayerController : MonoBehaviour
     }
 
     private void FixedUpdate()
-    { 
-
+    {
+        CheckLife();
     }
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    void CheckLife()
+    {
+        if (health <= 0)
+        {
+            OnPlayerDeath();
+        }
+    }
+
+    void OnDamageReceived(int damage)
+    {
+        this.health -= damage;
+    }
+
+    void OnHealing(int healing)
+    {
+        if (this.health + healing >= this.maxHealth)
+        {
+            this.health = maxHealth;
+            return;
+        }
+        this.health += healing;
+    }
+
+    void OnManaLost(int mana)
+    {
+        this.mana -= mana;
+    }
+
+    void OnManaReceived(int mana)
+    {
+        if (this.mana + mana >= this.maxMana)
+        {
+            this.mana = maxMana;
+            return;
+        }
+        this.mana += mana;
+    }
+    void OnStaminaLost(int stamina)
+    {
+        this.stamina -= stamina;
+    }
+
+    void OnStaminaReceived(int stamina)
+    {
+        if (this.stamina + stamina >= this.maxStamina)
+        {
+            this.stamina = this.maxStamina;
+            return;
+        }
+        this.stamina += stamina;
     }
 
 }
