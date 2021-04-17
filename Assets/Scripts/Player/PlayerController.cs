137d5d9ed6e88bedb724660b074cd67c1e50b8c9
Fabian Frangella <fabianfrangella@gmail.com>
2021-04-17 13:46:53 -0300

    Basic movement

--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -4,15 +4,21 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+
     // Start is called before the first frame update
     void Start()
     {
         
     }
 
+    private void FixedUpdate()
+    { 
+
+    }
     // Update is called once per frame
     void Update()
     {
         
     }
+
 }
--- Assets/Scripts/Player/PlayerMovement.cs
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -4,15 +4,26 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
+    public float speed;
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
+    private void FixedUpdate()
+    {
+        Move();
+    }
     // Update is called once per frame
     void Update()
     {
-        
+
+    }
+
+    private void Move()
+    {
+        var v3 = new Vector3(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"), 0);
+        transform.Translate(speed * v3.normalized * Time.deltaTime);
     }
 }
