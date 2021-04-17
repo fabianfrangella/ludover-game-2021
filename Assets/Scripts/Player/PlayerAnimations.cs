904964ded218aeb538b260169a8e89802f6351da
Fabian Frangella <fabianfrangella@gmail.com>
2021-04-17 18:38:16 -0300

    Agrega movimiento, animación y colisión de personaje

---
+++ Assets/Scripts/Player/PlayerAnimations.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerAnimations : MonoBehaviour
+{
+
+    public Animator animator;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    private void FixedUpdate()
+    {
+        SetRunAnimation();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void SetRunAnimation()
+    {
+        var horizontal = Input.GetAxisRaw("Horizontal");
+        var vertical = Input.GetAxisRaw("Vertical");
+        if (horizontal == 0 && vertical == 0)
+        {
+            animator.SetFloat("Speed", -1);
+            return;
+        }
+        if (Mathf.Abs(vertical) > 0)
+        {
+            animator.SetFloat("Speed", Mathf.Abs(vertical));
+            return;
+        }
+        if (Mathf.Abs(horizontal) > 0)
+        {
+            print(Mathf.Abs(horizontal));
+            animator.SetFloat("Speed", Mathf.Abs(horizontal));
+            return;
+        }
+    }
+}
