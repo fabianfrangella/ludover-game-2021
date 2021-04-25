5bc68cbd11e84decbc56574059a47c510fadfa28
Fabian Frangella <fabianfrangella@gmail.com>
2021-04-24 22:52:18 -0300

    walk animation

---
+++ Assets/Scripts/Enemy/EnemyMovementManager.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyMovementManager : MonoBehaviour
+{
+
+    public Rigidbody2D rb;
+    public float speed = 2.0f;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Move();
+    }
+
+    private void Move()
+    {
+        rb.velocity = DirectionWhereIsMoving().normalized * speed;
+    }
+
+    private float GetMoveDirection()
+    {
+        return 1;
+    }
+
+    public Vector3 DirectionWhereIsMoving()
+    {
+        return new Vector2(1, 0);
+    }
+}
--- Assets/Scripts/Enemy/SkeletonAnimationManager.cs
+++ Assets/Scripts/Enemy/SkeletonAnimationManager.cs
@@ -4,14 +4,21 @@ using UnityEngine;
 
 public class SkeletonAnimationManager : EnemyAnimationManager
 {
+
     public override void StartDieAnimation(float health)
     {
         animator.SetFloat("Health", health);
     }
 
+    public void SetIsIdle(bool isIdle)
+    {
+        animator.SetBool("isIdle", isIdle);
+    }
+
     // Start is called before the first frame update
     void Start()
     {
+        animator.SetBool("isIdle", false);
         animator = gameObject.GetComponent<Animator>();
     }
 
