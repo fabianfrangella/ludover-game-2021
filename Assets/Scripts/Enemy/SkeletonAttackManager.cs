88506c7e2fae569edad3dffc4d7a218932ad92af
Fabian Frangella <fabianfrangella@gmail.com>
2021-05-02 01:01:21 -0300

    Simple Attack

--- Assets/Scripts/Enemy/SkeletonAttackManager.cs
+++ Assets/Scripts/Enemy/SkeletonAttackManager.cs
@@ -1,26 +1,71 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
 public class SkeletonAttackManager : MonoBehaviour
 {
+
+    public float attackDistance;
+    public float damage;
+    public float attackRate = 5f;
+    private float nextAttackTime = 0f;
+    private bool hasFoundPlayer;
+    private SkeletonAnimationManager animationManager;
+    private EnemyMovementManager enemyMovementManager;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        animationManager = GetComponent<SkeletonAnimationManager>();
+        enemyMovementManager = GetComponent<EnemyMovementManager>();
     }
 
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
-        
+        if (hasFoundPlayer)
+        {
+            Attack();
+        }
+        Debug.DrawRay(transform.position, enemyMovementManager.GetDirectionWhereIsLooking().normalized, Color.red);
     }
 
     void Attack()
     {
-        // PlayAnimation
-        // DoRaycast onDirectionWhereIsLooking (se le puede pedir al movementManager)
-        // if isPlayer then player.OnDamageReceive(damage)
-        // SetNextAttackTime()
+        if (CanAttack())
+        {
+            DoAttack();
+        }
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        hasFoundPlayer = collision.collider.CompareTag(TagEnum.Player.ToString());
+    }
+
+    private void OnCollisionExit2D(Collision2D collision)
+    {
+        hasFoundPlayer = false;
+    }
+    private void DoAttack()
+    {
+        animationManager.PlayAttackAnimation();
+        RaycastHit2D[] hits = Physics2D.RaycastAll(transform.position, enemyMovementManager.GetDirectionWhereIsLooking().normalized, attackDistance);
+        foreach (var hit in hits)
+        {
+            if (hit.collider.CompareTag(TagEnum.Player.ToString()))
+            {
+                hit.collider.gameObject.GetComponent<PlayerHealthManager>().OnDamageReceived(damage);
+            }
+        }
+        SetNextAttackTime();
+    }
+
+    void SetNextAttackTime()
+    {
+        nextAttackTime = Time.time + 1f / attackRate;
+    }
+
+    private bool CanAttack()
+    {
+        return Time.time >= nextAttackTime;
     }
 }
