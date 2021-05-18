a9661571526279477320b36c7ba01eedb4ece96a
Fabian Frangella <fabianfrangella@gmail.com>
2021-05-17 21:15:28 -0300

    Modificado algoritmo de pathfinding con tree

--- Assets/Scripts/Enemy/Path.cs
+++ Assets/Scripts/Enemy/Path.cs
@@ -1,76 +1,31 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Numerics;
-using UnityEngine;
+﻿using UnityEngine;
 using Vector2 = UnityEngine.Vector2;
 
 namespace Enemy
 {
-    public class Path
+    public class PathTree
     {
-        public List<Vector2> path;
-        private Vector2 CurrentDirection { get; set; }
-        public Path()
+        public PathTree nextPath;
+        private Vector2 direction;
+        
+        public void GeneratePath(Vector2 from, Transform target, float speed)
         {
-            path = new List<Vector2>();
+            direction = GetNextDirection(from, target.position, speed);
+            if (direction == (Vector2) target.position) return;
+            nextPath = new PathTree();
+            nextPath.GeneratePath(direction, target, speed);
+            
         }
-
-        public Vector2 GetNextDirection()
+        public Vector2 GetDirection()
         {
-            if (path.Count <= 0) return Vector2.zero;
-            var next = path[0];
-            var tail = path.Skip(1);
-            path = new List<Vector2>(tail);
-            return next;
-        }
-
-        public bool HasFinishedPath()
-        {
-            var finished = path.Count <= 0 || path == null;
-            return finished;
+            return direction;
         }
         
-        public void GeneratePath(Vector2 from, Transform to, float speed)
-        {
-            path = new List<Vector2>();
-            var nextDir = GetNextDirection(from, to.position, speed);
-            CurrentDirection = from;
-            while ((Vector2) to.position != nextDir)
-            {
-                nextDir = GetNextDirection(CurrentDirection, to.position, speed);
-                path.Add(nextDir);
-            }
-            Debug.Log("THE PATH IS: " + GetPathString());
-        }
-
         private Vector2 GetNextDirection(Vector2 from, Vector2 to, float speed)
         {
             var nextDirection = Vector2.MoveTowards(from, to, speed);
-            if (PositionIsOcuppied(nextDirection))
-            {
-                nextDirection = GetNextDirection((Vector2) from, to - new Vector2(-1, -1), speed);
-            }
-            CurrentDirection = nextDirection;
             return nextDirection;
         }
-
-        private bool PositionIsOcuppied(Vector2 position)
-        {
-            return false;
-        }
-
-        public string GetPathString()
-        {
-            var pathS = "";
-            foreach (var dir in path)
-            {
-                pathS+=  "X: " + dir.x + " Y: " + dir.y;
-            }
-
-            return pathS;
-        }
-        
     }
 
     public class Seeker
@@ -78,17 +33,16 @@ namespace Enemy
         private Transform target;
         private Transform transform;
         private Vector2 initialTargetPosition;
-        private Vector2 lastDirection;
-        private Path path;
+        private PathTree pathTree;
 
         public Seeker(Transform target, Transform transform)
         {
             this.target = target;
             this.transform = transform;
             initialTargetPosition = target.position;
-            path = new Path();
+            pathTree = new PathTree();
             Debug.Log("GENERATING FIRST PATH");
-            path.GeneratePath(transform.position, target, 1f);
+            pathTree.GeneratePath(transform.position,target,1f);
         }
         
         public Vector2 GetNextDirectionTowardsTarget()
@@ -96,24 +50,15 @@ namespace Enemy
             if (TargetHasMoved())
             {
                 Debug.Log("GENERATING NEW PATH");
-                SetNewTarget(target);
+                initialTargetPosition = target.position;
+                pathTree.GeneratePath(transform.position,target,1f);
             }
-            if (path.HasFinishedPath())
-            {
-                return lastDirection;
-            }
-            lastDirection = path.GetNextDirection();
-            Debug.Log("NEXT DIRECTION: " + "{X: " + lastDirection.x + " Y: " + lastDirection.y + " }");
-            return lastDirection;
+            if (pathTree.nextPath == null) 
+                return pathTree.GetDirection();
+            
+            pathTree = pathTree.nextPath;
+            return pathTree.GetDirection();
         }
-
-        private void SetNewTarget(Transform newTarget)
-        {
-            target = newTarget;
-            initialTargetPosition = newTarget.position;
-            path.GeneratePath(transform.position, newTarget, 1f);
-        }
-        
         private bool TargetHasMoved()
         {
             return Vector2.Distance(target.position, initialTargetPosition) > 0.1f;
