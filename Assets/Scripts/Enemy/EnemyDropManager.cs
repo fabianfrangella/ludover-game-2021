71a1fb38e496cab2ba668b30f316f6e5f6d8f3b8
Fabian Frangella <fabianfrangella@gmail.com>
2021-05-22 21:05:24 -0300

    fix drops after death

--- Assets/Scripts/Enemy/EnemyDropManager.cs
+++ Assets/Scripts/Enemy/EnemyDropManager.cs
@@ -5,12 +5,15 @@ using UnityEngine;
 public class EnemyDropManager : MonoBehaviour
 {
     public List<ItemEnum> droppableItems;
+    public int numberOfDrops;
 
+    private int drops = 0;
     private ItemFactory itemFactory;
     private EnemyHealthManager enemyHealthManager;
 
     private void Start()
     {
+        numberOfDrops = 1;
         itemFactory = GetComponent<ItemFactory>();
         enemyHealthManager = GetComponent<EnemyHealthManager>();
         enemyHealthManager.OnDeath += DropItem;
@@ -18,12 +21,16 @@ public class EnemyDropManager : MonoBehaviour
 
     private void DropItem()
     {
-        var random = Random.Range(0, 2);
-        if (random < 1)
+        while (drops < numberOfDrops)
         {
-            Instantiate(itemFactory.GetItemPrefab(ItemEnum.HealthPotion), transform.position, Quaternion.identity);
-            return;
+            drops++;
+            var random = Random.Range(0, 2);
+            if (random < 1)
+            {
+                Instantiate(itemFactory.GetItemPrefab(ItemEnum.HealthPotion), transform.position, Quaternion.identity);
+                continue;
+            }
+            Instantiate(itemFactory.GetItemPrefab(ItemEnum.ManaPotion), transform.position, Quaternion.identity);
         }
-        Instantiate(itemFactory.GetItemPrefab(ItemEnum.ManaPotion), transform.position, Quaternion.identity);
     }
 }
