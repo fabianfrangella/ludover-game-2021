898c39ceb2187f8aaf387a67bc614ce7859535e5
Fabian Frangella <fabianfrangella@gmail.com>
2021-05-30 01:53:20 -0300

    Hints

---
+++ Assets/Scripts/Hud/Hints.cs
@@ -0,0 +1,22 @@
+﻿using TMPro;
+using UnityEngine;
+
+namespace Hud
+{
+    public class Hints : MonoBehaviour
+    {
+        public TextMeshProUGUI text;
+
+        private void Start()
+        {
+            text.enabled = false;
+        }
+        private void Update()
+        {
+            if (Input.GetKeyDown(KeyCode.H))
+            {
+                text.enabled = !text.isActiveAndEnabled;
+            }
+        }
+    }
+}
