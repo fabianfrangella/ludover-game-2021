1fc5ccf8f26a99a6d93046e367816e61a41d281a
Fabian Frangella <fabianfrangella@gmail.com>
2021-05-30 22:10:32 -0300

    added restar button

---
+++ Assets/Scripts/Hud/RestartGameButtonUI.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using TMPro;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+namespace Hud
+{
+    public class RestartGameButtonUI : MonoBehaviour
+    {
+        public Button button;
+        private PlayerHealthManager playerHealthManager;
+        private void Start()
+        {
+            button.enabled = false;
+            GetComponent<Image>().enabled = false;
+            transform.GetChild(0).GetComponent<TextMeshProUGUI>().enabled = false;
+            playerHealthManager = FindObjectOfType<PlayerHealthManager>();
+            playerHealthManager.OnPlayerDeath += EnableButton;
+        }
+
+        private void EnableButton()
+        {
+            button.enabled = true;
+            GetComponent<Image>().enabled = true;
+            transform.GetChild(0).GetComponent<TextMeshProUGUI>().enabled = true;
+        }
+
+        public void RestartGame()
+        {
+            SceneManager.LoadScene("GameScene");
+        }
+    }
+}
