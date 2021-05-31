79b87d9ed8651c03a83b0fe1470aa06b1e3ec5c3
Fabian Frangella <fabianfrangella@gmail.com>
2021-05-30 21:47:04 -0300

    agrega blood overlay

---
+++ Assets/Scripts/Hud/BloodOverlayUI.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace Hud
+{
+    public class BloodOverlayUI : MonoBehaviour
+    {
+        public Image image;
+
+        private PlayerStats playerStats;
+        private PlayerHealthManager playerHealthManager;
+        private bool hit;
+        private void Start()
+        {
+            playerStats = FindObjectOfType<PlayerStats>();
+            playerHealthManager = FindObjectOfType<PlayerHealthManager>();
+            playerHealthManager.OnHitReceived += DisplayOverlay;
+            image.enabled = false;
+        }
+
+        private void Update()
+        {
+            if (hit) return;
+            
+            var alpha = .5f - playerStats.health / playerStats.maxHealth;
+            image.enabled = playerStats.health < playerStats.maxHealth;
+            image.color = new Color(image.color.r, image.color.g, image.color.b,
+                alpha > 0.5f ? 0.5f : alpha);
+        }
+
+
+        private void DisplayOverlay()
+        {
+            StartCoroutine(nameof(DisplayOverlayRoutine));
+        }
+        private IEnumerator DisplayOverlayRoutine()
+        {
+            hit = true;
+            image.enabled = true;
+            image.color = new Color(image.color.r, image.color.g, image.color.b, .1f);
+            yield return new WaitForSeconds(0.5f);
+            hit = false;
+        }
+    }
+}
