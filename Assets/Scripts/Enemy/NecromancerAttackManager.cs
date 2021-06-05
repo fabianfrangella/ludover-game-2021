69fa386e56d7fbbcd34ef0968f5258be5988fd76
Fabian Frangella <fabianfrangella@gmail.com>
2021-06-05 18:20:51 -0300

    trigger

--- Assets/Scripts/Enemy/NecromancerAttackManager.cs
+++ Assets/Scripts/Enemy/NecromancerAttackManager.cs
@@ -11,14 +11,26 @@ namespace Enemy
         private InvokeSkeletons invokeSkeletons;
         private NecromancerAnimationManager animationManager;
         private EnemyHealthManager enemyHealthManager;
+        private bool hasBeenTriggered;
         private void Start()
         {
             invokeSkeletons = GetComponent<InvokeSkeletons>();
             animationManager = GetComponent<NecromancerAnimationManager>();
             enemyHealthManager = GetComponent<EnemyHealthManager>();
+            enemyHealthManager.OnHit += Trigger;
+            enemyHealthManager.SetAbsorption(10000);
         }
 
         private void Update()
+        {
+            if (hasBeenTriggered)
+            {
+                InvokeSkeletons();
+            }
+            
+        }
+
+        private void InvokeSkeletons()
         {
             if (enemyHealthManager.IsAlive() && !AreChildrenAlive() && invokesDone != invokes)
             {
@@ -27,7 +39,6 @@ namespace Enemy
                 invokeSkeletons.Invoke();
                 invokesDone++;
             }
-
             if (!AreChildrenAlive())
             {
                 enemyHealthManager.SetAbsorption(0);
@@ -37,6 +48,11 @@ namespace Enemy
         private bool AreChildrenAlive()
         {
             return invokeSkeletons.AreChildrenAlive();
+        }
+
+        private void Trigger()
+        {
+            hasBeenTriggered = true;
         }
     }
 }
