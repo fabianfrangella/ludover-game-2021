0a9ca49da13a23ccd5086143d5b04c352dcf0053
Fabian Frangella <fabianfrangella@gmail.com>
2021-06-05 17:30:05 -0300

    invoke spell

---
+++ Assets/Scripts/Enemy/NecromancerAttackManager.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using Spells;
+using UnityEngine;
+
+namespace Enemy
+{
+    public class NecromancerAttackManager : MonoBehaviour
+    {
+        public int invokes = 2;
+        private const int InvokesDone = 0;
+        private InvokeSkeletons invokeSkeletons;
+        private void Start()
+        {
+            invokeSkeletons = GetComponent<InvokeSkeletons>();
+        }
+
+        private void Update()
+        {
+            if (!AreChildrenAlive() && InvokesDone != invokes)
+            {
+                invokeSkeletons.Invoke();
+            }
+        }
+
+        private bool AreChildrenAlive()
+        {
+            return transform.childCount > 0;
+        }
+    }
+}
---
+++ Assets/Scripts/Spells/InvokeSkeletons.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Spells
+{
+    public class InvokeSkeletons : MonoBehaviour
+    {
+        public Transform skeleton;
+
+        public void Invoke()
+        {
+            var position = transform.position;
+
+            var skeletons =
+                new List<Transform>
+                {
+                    Instantiate(skeleton, new Vector2(position.x + 1, position.y + 1), Quaternion.identity),
+                    Instantiate(skeleton, new Vector2(position.x - 1, position.y - 1), Quaternion.identity),
+                    Instantiate(skeleton, new Vector2(position.x + 1, position.y - 1), Quaternion.identity),
+                    Instantiate(skeleton, new Vector2(position.x - 1, position.y + 1), Quaternion.identity),
+                    Instantiate(skeleton, new Vector2(position.x + 1.5f, position.y + 1.5f), Quaternion.identity),
+                    Instantiate(skeleton, new Vector2(position.x - 1.5f, position.y - 1.5f), Quaternion.identity),
+                    Instantiate(skeleton, new Vector2(position.x + 1.5f, position.y - 1.5f), Quaternion.identity),
+                    Instantiate(skeleton, new Vector2(position.x - 1.5f, position.y + 1.5f), Quaternion.identity)
+                };
+            foreach (var s in skeletons)
+            {
+                s.parent = transform;
+            }
+        }
+    }
+}
