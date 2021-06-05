d8b1b2c2b9f428bdd35f1c591131c8310b3a3b02
Fabian Frangella <fabianfrangella@gmail.com>
2021-06-05 17:10:00 -0300

    animations and health manager

---
+++ Assets/Scripts/Enemy/NecromancerAnimationManager.cs
@@ -0,0 +1,51 @@
+﻿using UnityEngine;
+
+namespace Enemy
+{
+    public class NecromancerAnimationManager : EnemyAnimationManager
+    {
+        public float timeToDisappearBody = 5;
+        private bool isDead = false;
+        private float timeSinceDeath = 0;
+        
+        private void Start()
+        {
+            animator = gameObject.GetComponent<Animator>();
+        }
+
+        // Update is called once per frame
+        private void Update()
+        {
+            if (isDead)
+            {
+                HandleDeath();
+            }
+        }
+        public override void StartDieAnimation(float health)
+        {
+            isDead = health <= 0;
+            if (isDead)
+            {
+                animator.SetTrigger("Die");
+            }
+        }
+        
+        public override void PlayAttackAnimation()
+        {
+            animator.SetTrigger("Cast");
+        }
+        
+        private void HandleDeath()
+        {
+            if (timeSinceDeath < timeToDisappearBody)
+            {
+                timeSinceDeath += Time.deltaTime;
+            }
+            if (timeSinceDeath > timeToDisappearBody)
+            {
+                gameObject.SetActive(false);
+            }
+        }
+    }
+    
+}
