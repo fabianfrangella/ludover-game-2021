dfa800bbaf449918aed70d6afa690e680fcea9e8
Fabian Frangella <fabianfrangella@gmail.com>
2021-06-28 01:00:37 -0300

    Loading Screen

---
+++ Assets/Scripts/Menu/LoadingScreenTip.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections;
+using TMPro;
+using UnityEngine;
+using Random = UnityEngine.Random;
+
+namespace Menu
+{
+    public class LoadingScreenTip : MonoBehaviour
+    {
+        public TextMeshProUGUI tmp;
+        private float tipTime = 5f;
+        private float timeElapsed = 0;
+        [SerializeField]
+        public string[] tips;
+
+        private void Start()
+        {
+            tmp.text = tips[Random.Range(0, tips.Length)];
+        }
+
+        private void Update()
+        {
+            timeElapsed += Time.deltaTime;
+            if (timeElapsed > tipTime)
+            {
+                tmp.text = tips[Random.Range(0, tips.Length)];
+                timeElapsed = 0;
+            }
+        }
+        
+    }
+}
