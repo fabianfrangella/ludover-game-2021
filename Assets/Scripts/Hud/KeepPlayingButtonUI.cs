4e2a045ec9367cdc00fc15644171ac053bbd31ff
Fabian Frangella <fabianfrangella@gmail.com>
2021-06-30 12:37:44 -0300

    Agrega menu de victoria

---
+++ Assets/Scripts/Hud/KeepPlayingButtonUI.cs
@@ -0,0 +1,38 @@
+﻿using Enemy;
+using TMPro;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+namespace Hud
+{
+    public class KeepPlayingButtonUI : MonoBehaviour
+    {
+        public Button button;
+
+        private EndGameManager endGameManager;
+        private void Start()
+        {
+            button.enabled = false;
+            GetComponent<Image>().enabled = false;
+            transform.GetChild(0).GetComponent<TextMeshProUGUI>().enabled = false;
+            endGameManager = FindObjectOfType<EndGameManager>();
+            if (endGameManager != null)
+            {
+                endGameManager.OnGameEnd += EnableButton;
+            }
+        }
+        
+        private void EnableButton()
+        {
+            button.enabled = true;
+            GetComponent<Image>().enabled = true;
+            transform.GetChild(0).GetComponent<TextMeshProUGUI>().enabled = true;
+        }
+        
+        public void KeepPlaying()
+        {
+            transform.parent.gameObject.SetActive(false);
+        }
+    }
+}
---
+++ Assets/Scripts/Hud/RestartOnFinishButton.cs
@@ -0,0 +1,39 @@
+﻿using Enemy;
+using TMPro;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+namespace Hud
+{
+    public class RestartOnFinishButton : MonoBehaviour
+    {
+        public Button button;
+
+        private EndGameManager endGameManager;
+        private void Start()
+        {
+            button.enabled = false;
+            GetComponent<Image>().enabled = false;
+            transform.GetChild(0).GetComponent<TextMeshProUGUI>().enabled = false;
+            endGameManager = FindObjectOfType<EndGameManager>();
+            if (endGameManager != null)
+            {
+                endGameManager.OnGameEnd += EnableButton;
+            }
+        }
+        
+        private void EnableButton()
+        {
+            Debug.Log("Can restart now");
+            button.enabled = true;
+            GetComponent<Image>().enabled = true;
+            transform.GetChild(0).GetComponent<TextMeshProUGUI>().enabled = true;
+        }
+        
+        public void Restart()
+        {
+            SceneManager.LoadScene("SafeZone");
+        }
+    }
+}
