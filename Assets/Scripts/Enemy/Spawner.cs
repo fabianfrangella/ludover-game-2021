8ac2c84810c025458ad4c3c59f5ed8d4601e6bec
Fabian Frangella <fabianfrangella@gmail.com>
2021-07-03 17:32:25 -0300

    Skeleton spawner

---
+++ Assets/Scripts/Enemy/Spawner.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+using Random = UnityEngine.Random;
+
+namespace Enemy
+{
+    public class Spawner : MonoBehaviour
+    {
+        public Transform character;
+
+        public int maxAlive;
+        public int alive;
+        public Vector2 xRange;
+        public Vector2 yRange;
+        private void Start()
+        {
+            maxAlive = 10;
+            alive = 0;
+            Spawn();
+        }
+        private void Update()
+        {
+            if (alive < maxAlive)
+            {
+                Spawn();
+            }
+        }
+
+        private void Spawn()
+        {
+            while (alive < maxAlive)
+            {
+                var pos = new Vector2(Random.Range(xRange.x, xRange.y), Random.Range(yRange.x, yRange.y));
+                var spawn = Instantiate(character, pos, Quaternion.identity);
+                Debug.Log("x " + spawn.position.x + " " + " y" +  spawn.position.y);
+                spawn.GetComponent<EnemyHealthManager>().OnDeath += HandleDeath;
+                spawn.GetComponent<EnemyPathFinder>().state = EnemyPathFinder.State.WANDERING;
+                alive++;
+            }
+        }
+
+        private void HandleDeath()
+        {
+            alive--;
+        }
+        
+    }
+}
