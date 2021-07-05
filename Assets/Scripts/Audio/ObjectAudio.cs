49db06bcbe2cc1d0c3eacfa7ff44c5db02600ce6
Fabian Frangella <fabianfrangella@gmail.com>
2021-07-05 20:48:41 -0300

    campfire sound

---
+++ Assets/Scripts/Audio/ObjectAudio.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using UnityEngine;
+
+namespace Audio
+{
+    public class ObjectAudio : MonoBehaviour
+    {
+        public string clip;
+        private AudioManager audioManager;
+
+        private void Start()
+        {
+            audioManager = FindObjectOfType<AudioManager>();
+            audioManager.Play(clip);
+        }
+        
+    }
+}
