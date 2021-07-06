f3cefe305784da3e34a84e1a5d7e5e6616bef56a
Fabian Frangella <fabianfrangella@gmail.com>
2021-07-05 21:07:22 -0300

    fire sound

--- Assets/Scripts/Audio/ObjectAudio.cs
+++ Assets/Scripts/Audio/ObjectAudio.cs
@@ -5,13 +5,21 @@ namespace Audio
 {
     public class ObjectAudio : MonoBehaviour
     {
-        public string clip;
-        private AudioManager audioManager;
+        public Sound sound;
 
         private void Start()
         {
-            audioManager = FindObjectOfType<AudioManager>();
-            audioManager.Play(clip);
+            sound.source = gameObject.AddComponent<AudioSource>();
+            sound.source.clip = sound.clip;
+            sound.source.volume = sound.volume;
+            sound.source.pitch = sound.pitch;
+            sound.source.loop = sound.loop;
+            sound.source.spatialBlend = 1;
+            sound.source.rolloffMode = AudioRolloffMode.Linear;
+            sound.source.minDistance = 1;
+            sound.source.maxDistance = 10.5f;
+            
+            sound.source.Play();
         }
         
     }
