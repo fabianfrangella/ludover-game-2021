df058c475a287a0be6c53b56609a0653ff73d32b
Fabian Frangella <fabianfrangella@gmail.com>
2021-07-15 16:58:50 -0300

    dialog

---
+++ Assets/Scripts/NPC/Dialog.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections;
+using UnityEngine;
+
+namespace NPC
+{
+    public class Dialog : MonoBehaviour
+    {
+        [SerializeField] public Canvas dialog;
+        private bool canInteract = false;
+
+        private void Start()
+        {
+            dialog.gameObject.SetActive(false);
+        }
+
+        private void Update()
+        {
+            if (canInteract && Input.GetKeyDown(KeyCode.F))
+            {
+                EnableDialog();
+            } 
+        }
+
+        private void EnableDialog()
+        {
+            dialog.gameObject.SetActive(true);
+        }
+
+        private IEnumerator DisableDialog()
+        {
+            yield return new WaitForSeconds(2);
+            dialog.gameObject.SetActive(false);
+        }
+        private void OnCollisionEnter2D(Collision2D other)
+        {
+            canInteract = other.collider.CompareTag("Player");
+        }
+
+        private void OnCollisionExit2D(Collision2D other)
+        {
+            if (other.collider.CompareTag("Player"))
+            {
+                canInteract = false;
+                StartCoroutine(nameof(DisableDialog));
+            }
+        }
+    }
+}
